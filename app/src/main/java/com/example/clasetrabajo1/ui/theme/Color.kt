9ac180fdc9c9451079684045412d631b6c74bf4d
MajoEsparza <22151136@aguascalientes.tecnm.mx>
2025-04-01 23:23:09 -0600

    Adding components, navigation between interfaces, images, primary and secondary colors.

--- app/src/main/java/com/example/clasetrabajo1/ui/theme/Color.kt
+++ app/src/main/java/com/example/clasetrabajo1/ui/theme/Color.kt
@@ -1,11 +1,25 @@
 package com.example.clasetrabajo1.ui.theme
 
+import android.hardware.lights.Light
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val Grey = Color(0xFFF1F1F1)
+//0xFFC5C0C0
+
+// Light Blue
+val LightBlue50 = Color(0xFFE1F5FE)
+val LightBlue100 = Color(0xFFB3E5FC)
+val LightBlue200 = Color(0xFF81D4FA)
+val LightBlue300 = Color(0xFF4FC3F7)
+val LightBlue400 = Color(0xFF29B6F6)
+val LightBlue500 = Color(0xFF03A9F4)
+val LightBlue600 = Color(0xFF039BE5)
+val LightBlue700 = Color(0xFF0288D1)
+val LightBlue800 = Color(0xFF0277BD)
+val LightBlue900 = Color(0xFF01579B)
